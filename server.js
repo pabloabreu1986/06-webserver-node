6885482deecafe1727a0d1c7434c5a5d30a5d26e
Abreu Fernandez <pablo.abreufernandez@cencosud.cl>
2018-08-03 17:37:54 -0400

    fix: about name

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
 });
  
 app.get('/about', (req, res) => {
-    res.render('about');  
+    res.render('about',{
+        nombre: 'pablo luis'
+    });  
 });
 
 app.listen(port, () => {
